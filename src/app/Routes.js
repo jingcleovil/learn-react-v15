0af1b770ada5796b3f666b2e48fd354e42fc4c82
Jinggo Villamor <jinggo.villamor@gmail.com>
2016-06-25 07:26:17 +0800

    Update routes

--- src/app/Routes.js
+++ src/app/Routes.js
@@ -3,22 +3,21 @@ import { Provider } from 'react-redux';
 import { syncHistoryWithStore } from 'react-router-redux';
 import Route from 'react-router/lib/Route';
 import Router from 'react-router/lib/Router';
+import browserHistory from 'react-router/lib/browserHistory';
 import IndexRoute from 'react-router/lib/IndexRoute';
-
-import browserHistory from './middleware/history';
 import configureStore from './Stores';
 
 const store = configureStore();
 const history = syncHistoryWithStore(browserHistory, store);
 
-import Layout from './Layout';
+import App from './App';
 
 class Routes extends Component {
     render() {
         return (
             <Provider store={ store }>
                 <Router history={ history }>
-                    <Route path="/" component={ Layout }>
+                    <Route path="/(:page)" component={ Layout }>
                         
                     </Route>
                 </Router>
