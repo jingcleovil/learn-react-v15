609d2867d65068453dd0fc57b2e75a00ce62b069
Jinggo Villamor <jinggo.villamor@gmail.com>
2016-06-25 18:58:59 +0800

    instead of list-group use table for the list of index

--- src/app/todo/components/TodoList.js
+++ src/app/todo/components/TodoList.js
@@ -1,13 +1,27 @@
 import React, { PropTypes } from 'react';
 
 const TodoList = ({ todos }) => (
-    <ul className="list-group">
-        { todos.map(todo => 
-            <li key={ todo.id } className="list-group-item">
-                <a href="">{ todo.text }</a>
-            </li>
-        )}
-    </ul>
+    <table className="table table-bordered">
+        <thead>
+            <tr>
+                <th width="25%">ID</th>
+                <th>Task Name</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            { todos.map(todo =>
+                <tr key={ todo.id }>
+                    <td>{ todo.id }</td>
+                    <td>{ todo.text }</td>
+                    <td>
+                        <a href="" className="btn btn-xs btn-danger">Complete</a>
+                    </td>
+                </tr>
+            )}
+            
+        </tbody>
+    </table>
 )
 
 export default TodoList
